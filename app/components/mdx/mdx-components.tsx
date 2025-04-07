ef44adec97e8b741662bd2a37465775ce2f60813
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-04-07 08:00:49 +0200

    Cleanup and use catalyst components in mdx.

--- app/components/mdx/mdx-components.tsx
+++ app/components/mdx/mdx-components.tsx
@@ -6,6 +6,10 @@ import {
 	GradientTitleMdx,
 	type GradientTitleMdxProps,
 } from "../ui/gradient-title";
+import { Code, Strong, Text, TextLink } from "../catalyst/text";
+import type { Link } from "@/components/catalyst/link";
+import { Divider } from "../catalyst/divider";
+import { Table, TableCell, TableHeader, TableRow } from "../catalyst/table";
 
 const components = {
 	h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
@@ -62,17 +66,11 @@ const components = {
 			{...props}
 		/>
 	),
-	a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
-		<a
-			className={cn("font-medium underline underline-offset-4", className)}
-			{...props}
-		/>
+	a: ({ className, ...props }: React.ComponentPropsWithoutRef<typeof Link>) => (
+		<TextLink className={className} {...props} />
 	),
-	p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
-		<p
-			className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
-			{...props}
-		/>
+	p: ({ className, ...props }: React.ComponentPropsWithoutRef<"p">) => (
+		<Text className={className} {...props} />
 	),
 	ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
 		<ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
@@ -106,41 +104,32 @@ const components = {
 			className={cn("rounded-md border", className)}
 		/>
 	),
-	hr: ({ ...props }) => <hr className="my-4 md:my-8" {...props} />,
-	table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
-		<div className="my-6 w-full overflow-y-auto">
-			<table className={cn("w-full", className)} {...props} />
-		</div>
+	hr: ({ ...props }: React.ComponentPropsWithoutRef<typeof Divider>) => (
+		<Divider {...props} soft />
 	),
-	tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-		<tr
-			className={cn("m-0 border-t p-0 even:bg-muted", className)}
-			{...props}
-		/>
+	table: ({
+		className,
+		...props
+	}: React.ComponentPropsWithoutRef<typeof Table>) => (
+		<Table className={className} {...props} />
+	),
+	tr: ({
+		className,
+		...props
+	}: React.ComponentPropsWithoutRef<typeof TableRow>) => (
+		<TableRow className={className} {...props} />
 	),
 	th: ({
 		className,
 		...props
-	}: React.HTMLAttributes<HTMLTableHeaderCellElement>) => (
-		<th
-			className={cn(
-				"border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
-				className,
-			)}
-			{...props}
-		/>
+	}: React.ComponentPropsWithoutRef<typeof TableHeader>) => (
+		<TableHeader className={className} {...props} />
 	),
 	td: ({
 		className,
 		...props
-	}: React.HTMLAttributes<HTMLTableDataCellElement>) => (
-		<td
-			className={cn(
-				"border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
-				className,
-			)}
-			{...props}
-		/>
+	}: React.ComponentPropsWithoutRef<typeof TableCell>) => (
+		<TableCell className={className} {...props} />
 	),
 	pre: ({ className, ...props }: React.HTMLAttributes<HTMLPreElement>) => (
 		<pre
@@ -151,14 +140,14 @@ const components = {
 			{...props}
 		/>
 	),
-	code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-		<code
-			className={cn(
-				"relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm",
-				className,
-			)}
-			{...props}
-		/>
+	code: ({ className, ...props }: React.ComponentPropsWithoutRef<"code">) => (
+		<Code className={className} {...props} />
+	),
+	strong: ({
+		className,
+		...props
+	}: React.ComponentPropsWithoutRef<"strong">) => (
+		<Strong className={className} {...props} />
 	),
 	GradientTitleMdx: ({ className, ...props }: GradientTitleMdxProps) => (
 		<GradientTitleMdx className={cn(className)} {...props} />
