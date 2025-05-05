fe4030bb62fd54f99233f692bed58debd836db68
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-05-05 06:15:07 +0200

    Make long title test

--- app/routes/articles/$slug.tsx
+++ app/routes/articles/$slug.tsx
@@ -2,7 +2,6 @@ import { createFileRoute, useLoaderData } from "@tanstack/react-router";
 import { allArticles } from "content-collections";
 import { GradientTitle } from "@/components/ui/gradient-title";
 import { Mdx } from "@/components/mdx/mdx-components";
-import { Badge } from "@/components/catalyst/badge";
 import { cn } from "@/lib/utils";
 export const Route = createFileRoute("/articles/$slug")({
 	component: RouteComponent,
