a8ba50e6c91328a2ac08ed7ae909f421f9d8d25d
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-05-12 21:22:10 +0200

    Update index.tsx

--- app/routes/playground/index.tsx
+++ app/routes/playground/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/playground/")({
 function RouteComponent() {
 	return (
 		<div className="relative flex h-full flex-1 flex-col items-center justify-center gap-4">
-			<div className="-z-10 mask-radial-at-center mask-radial-from-10% absolute inset-0 h-full w-full bg-[radial-gradient(circle,#73737360_0.1px,transparent_1px)] bg-[size:8px_8px]" />
+			<div className="-z-10 mask-radial-at-center mask-radial-from-20% absolute inset-0 h-full w-full bg-[radial-gradient(circle,#73737350_0.1px,transparent_1px)] bg-[size:8px_8px]" />
 			<Card />
 		</div>
 	);
