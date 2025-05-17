46745bd99134fbda0223e75379d2a151438de0c1
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-05-17 15:36:32 +0200

    Use new components in about page.

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,3 +1,6 @@
+import { Box } from "@/components/every-layout-components/Box";
+import { Center } from "@/components/every-layout-components/Center";
+import { Cluster } from "@/components/every-layout-components/Cluster";
 import { Body } from "@/components/ui/Typography/Body";
 import { ContentContainer } from "@/components/ui/content-container";
 import { FadeUp } from "@/components/ui/fade-up";
@@ -11,7 +14,7 @@ export const Route = createFileRoute("/about/")({
 
 function RouteComponent() {
 	return (
-		<ContentContainer className="max-w-prose">
+		<Center>
 			<GradientTitle primaryColor="#f12711" secondaryColor="#f5af19">
 				Developer. Father. Creator.
 			</GradientTitle>
@@ -21,52 +24,64 @@ function RouteComponent() {
 				journey through teaching and creating. I help others transition into
 				tech while building useful things along the way.
 			</Body>
-			<div className="mt:2 grid items-center gap-2 sm:grid-cols-2 sm:gap-6 md:items-center">
-				<motion.img
-					initial={{
-						opacity: 0,
-						filter: "blur(10px) brightness(20%) grayscale(100%)",
-						scale: 0.7,
-						rotate: -30,
-					}}
-					animate={{
-						opacity: 1,
-						filter: "blur(0px) brightness(130%) grayscale(100%)",
-						scale: [0.7, 1.1, 1],
-						rotate: 0,
-					}}
-					transition={{ duration: 0.4 }}
-					alt="Daniel Noworyta"
-					src="/images/avatar.webp"
-					width="336"
-					height="336"
-					className={cn("mx-auto aspect-square w-full rounded-lg object-cover")}
-				/>
+			<Cluster clusterSpace="12px">
+				<Box
+					className="w-full sm:w-[calc(50%-(var(--cluster-space)/2))]"
+					padding="0px"
+					borderWidth="0"
+				>
+					<motion.img
+						initial={{
+							opacity: 0,
+							filter: "blur(10px) brightness(20%) grayscale(100%)",
+							scale: 0.7,
+							rotate: -30,
+						}}
+						animate={{
+							opacity: 1,
+							filter: "blur(0px) brightness(130%) grayscale(100%)",
+							scale: [0.7, 1.1, 1],
+							rotate: 0,
+						}}
+						transition={{ duration: 0.4 }}
+						alt="Daniel Noworyta"
+						src="/images/avatar.webp"
+						width="336"
+						height="336"
+						className={cn("mx-auto aspect-square rounded-lg object-cover")}
+					/>
+				</Box>
 
-				<FadeUp delay={0.2} className="self-start">
-					<Body>
-						<strong>Hey, I&apos;m Daniel Noworyta.</strong> For most of my life,
-						I pushed aside my dream of becoming a programmer and doing what I
-						love. In 2020, I fulfilled that dream and became a programmer.
-					</Body>
-					<Body>
-						I have a{" "}
-						<a
-							href="https://youtube.com/c/danielnoworyta"
-							className="underline"
-						>
-							channel
-						</a>{" "}
-						on YouTube where I share my insights and experiences in learning
-						programming, transitioning to new fields, and advancing my career in
-						IT.
-					</Body>
-					<Body>
-						I love dark mode and side projects. When I&apos;m not working, I
-						like playing football, reading books, and eating pizza.
-					</Body>
-				</FadeUp>
-			</div>
-		</ContentContainer>
+				<Box
+					className="w-full sm:w-[calc(50%-(var(--cluster-space)/2))]"
+					borderWidth="0"
+				>
+					<FadeUp delay={0.2}>
+						<Body>
+							<strong>Hey, I&apos;m Daniel Noworyta.</strong> For most of my
+							life, I pushed aside my dream of becoming a programmer and doing
+							what I love. In 2020, I fulfilled that dream and became a
+							programmer.
+						</Body>
+						<Body>
+							I have a{" "}
+							<a
+								href="https://youtube.com/c/danielnoworyta"
+								className="underline"
+							>
+								channel
+							</a>{" "}
+							on YouTube where I share my insights and experiences in learning
+							programming, transitioning to new fields, and advancing my career
+							in IT.
+						</Body>
+						<Body>
+							I love dark mode and side projects. When I&apos;m not working, I
+							like playing football, reading books, and eating pizza.
+						</Body>
+					</FadeUp>
+				</Box>
+			</Cluster>
+		</Center>
 	);
 }
