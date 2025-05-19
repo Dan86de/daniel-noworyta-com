97b8861b00487c87b7d1187485250ff326becc26
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-05-19 07:04:08 +0200

    Fixed center

--- app/routes/articles/$slug.tsx
+++ app/routes/articles/$slug.tsx
@@ -19,7 +19,7 @@ function RouteComponent() {
 	const article = useLoaderData({ from: "/articles/$slug" });
 
 	return (
-		<Center maxWidth={"75ch"} gutters={4} className="w-[75ch]">
+		<Center gutters={4} className="w-[75ch]">
 			<Stack space="0.75rem">
 				<Box borderWidth="0" padding="0">
 					<GradientTitle primaryColor="#f12711" secondaryColor="#f5af19">
--- app/routes/playground/index.tsx
+++ app/routes/playground/index.tsx
@@ -10,7 +10,7 @@ function RouteComponent() {
 	return (
 		<Cover className="flex-grow" minHeight="100%">
 			<div className="-z-10 mask-radial-at-center mask-radial-from-20% absolute inset-0 h-full w-full bg-[radial-gradient(circle,#73737350_0.1px,transparent_1px)] bg-[size:8px_8px]" />
-			<Center maxWidth={"40ch"}>
+			<Center>
 				<Card />
 			</Center>
 		</Cover>
