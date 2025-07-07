9e5ac6d28057682805c52d8f30612ea64bd0af6f
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-07-07 18:30:43 +0200

    Broken state with cover

--- src/routes/playground/index.tsx
+++ src/routes/playground/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { Cover } from "@/components/every-layout-components/Cover";
+import { D3Button } from "@/components/playground/3d-button/3d-button";
 
 export const Route = createFileRoute("/playground/")({
 	component: RouteComponent,
@@ -9,7 +10,7 @@ function RouteComponent() {
 	return (
 		<Cover className="flex-grow" minHeight="100%">
 			<div className="-z-10 mask-radial-at-center mask-radial-from-20% absolute inset-0 h-full w-full bg-[radial-gradient(circle,#73737350_0.1px,transparent_1px)] bg-[size:8px_8px]" />
-			Playground
+			<D3Button />
 		</Cover>
 	);
 }
