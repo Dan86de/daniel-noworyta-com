05a7dfaa5e6be4cc2c49bfe3e61b76a8f2ffb825
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-08-03 15:44:23 +0200

    Add YT Button

--- src/components/catalyst/alert.tsx
+++ src/components/catalyst/alert.tsx
@@ -1,7 +1,7 @@
 import * as Headless from "@headlessui/react";
 import clsx from "clsx";
-import { Text } from "./text";
 import type React from "react";
+import { Text } from "./text";
 
 const sizes = {
 	xs: "sm:max-w-xs",
